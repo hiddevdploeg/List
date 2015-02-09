d845e023b74cabc2d71ecb823c766a1fd4ce5216
Hidde van der Ploeg <hiddevanderploeg@me.com>
2015-02-10 00:00:55 +0100

    Added basic List widget thing

---
+++ Add to List Widget/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  Add to List Widget
+//
+//  Created by Hidde van der Ploeg on 09-02-15.
+//  Copyright (c) 2015 Octoo Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
